906a573d3f21f30eaf709d66c9569dfcbb555b14
Alexander Serdobincev <alexanderser@yandex.ru>
2019-06-28 17:58:48 +0300

    Added product page and it's test

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,8 @@ class LoginPageLocators:
     LOGIN_URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'price_color')
---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+
+class ProductPage(BasePage):
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), 'No "Add to basket" button'
+
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+        self.solve_quiz_and_get_code()
+
+    def should_be_product_name_match(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        p_name = self.browser.find_elements(By.CSS_SELECTOR, '.alertinner')[0].find_element_by_tag_name('strong').text
+        assert product_name == p_name, 'Product names do not match'
+
+    def should_be_product_price_match(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        p_price = self.browser.find_elements(By.CSS_SELECTOR, '.alertinner')[2].find_element_by_tag_name('strong').text
+        assert product_price == p_price, 'Product names do not match'
+
